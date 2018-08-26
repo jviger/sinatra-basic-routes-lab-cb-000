42f08acc35754718a696f186a3fd79dd6d05c8b2
Jason Viger <jason.viger@gmail.com>
2018-08-26 10:19:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,4 +8,12 @@ class App < Sinatra::Base
    get '/name' do
     "Started my server using Shotgun!"
   end
+  
+  get '/hometown' do
+    "Started my server using Shotgun!"
+  end
+  
+  get '/favorite-song do
+    "Started my server using Shotgun!"
+  end
 end
